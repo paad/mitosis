b231ead868b4386a0ef27138f2fb80fe4b891986
Pascal Adam <pascal.adam@mhp.com>
2025-03-27 10:47:53 +0100

    initial commit

---
+++ library/mitosis.config.cjs
@@ -0,0 +1,21 @@
+
+/**
+ * @type {import('@builder.io/mitosis').MitosisConfig}
+ */
+module.exports = {
+  "files": "src/**",
+  "targets": [
+    "react"
+  ],
+  "dest": "packages",
+  "commonOptions": {
+    "typescript": true
+  },
+  "options": {
+    "react": {
+      "stylesType": "style-tag"
+    },
+    "svelte": {},
+    "qwik": {}
+  }
+}
---
+++ test-apps/react/src/App.jsx
@@ -0,0 +1,13 @@
+import { AutoComplete, Todos } from '@mitosis/library-react';
+
+function App() {
+  return (
+    <div>
+      <div>Welcome to React.</div>
+      <AutoComplete />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
